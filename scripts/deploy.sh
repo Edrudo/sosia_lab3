670a3a91ce49bcd59209149846367abe10d9f366
eduard.duras <eduard.duras@gmail.com>
2022-06-03 20:49:59 +0200

    Fixed scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@ set -e
 rm -rf "./production"
 mkdir -p "./production"
 cp -r ./test/sosia_lab3/* ./production
+git config --global user.email "eduard.duras@gmail.com"
+git config --global user.name "Edrudo"
 git add .
 git commit -m "Deployment from Jenkins"
 git push https://Edrudo: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@github.com/Edrudo/sosia_lab3.git
