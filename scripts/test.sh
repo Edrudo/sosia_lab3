d8746563e5d19fecaa2198daf9a8b16375a94f1a
eduard.duras <eduard.duras@gmail.com>
2022-06-03 15:38:59 +0200

    Broken version

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-mkdir -p "./logs"
 cd "./test/sosia_lab3/"
+mkdir "./logs"
 ls
 timestamp=$(date +%F_%H-%M-%S)
 ext=0
 if ! bandit -r ./test.py ./OperationsManager.py
 then
   banditLogFile=banditLog.$timestamp
-  touch ../logs/$banditLogFile
-  bandit -r ./test.py ./OperationsManager.py > ../logs/$banditLogFile
+  echo $PWD
+  touch ./logs/$banditLogFile
+  bandit -r ./test.py ./OperationsManager.py > ./logs/$banditLogFile
   ext=1
 fi
 
 if ! python3.8 ./test.py
 then
   testLogFile=testsLog.$timestamp
-  touch ../logs/$testLogFile
-  python3.8 ./test.py &> ../logs/$testLogFile
+  touch ./logs/$testLogFile
+  python3.8 ./test.py &> ./logs/$testLogFile
   ext=1
 fi
 
