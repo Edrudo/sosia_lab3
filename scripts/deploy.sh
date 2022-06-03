0efbe17b37ae849d3be0e1724a7e3511695e7d94
eduard.duras <eduard.duras@gmail.com>
2022-06-03 16:41:08 +0200

    Deployment from Jenkins

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ mkdir -p "./production"
 cp -r ./test/sosia_lab3/* ./production
 git add .
 git commit -m "Deployment from Jenkins"
-git push https://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@github.com/Edrudo/sosia_lab3.git
+git push https://Edrudo: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@github.com/Edrudo/sosia_lab3.git
